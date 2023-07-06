97656bad2a9b3bdeff66b20d7a1bd40e00a480cf
Maaacs <msb@icomp.ufam.edu.br>
2023-07-05 22:23:39 -0400

    Update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CG
+
+Instale a biblioteca OpenGL: sudo apt-get install freeglut3-dev
+
+
+Para compilar use:
+
+```bash
+g++ cilindro_opengl.cpp -o cilindro_opengl -lglut -lGL -lGLU
+```
--- cone.cpp
+++ cone.cpp
@@ -6,7 +6,8 @@ const double PI = 3.14159;
 const int p = 50;
 const int q = 50;
 
-// Funções f, g e h para cálculo das coordenadas dos pontos no cilindro
+
+// Funções f, g e h para cálculo das coordenadas dos pontos no cone
 float f(int i, int j)
 {
     return (cos((-1 + 2 * (float)i / p) * PI));
@@ -22,35 +23,35 @@ float h(int i, int j)
     return (-1 + 2 * (float)j / q);
 }
 
-// Função para desenhar o cilindro
-void drawCylinder(double raio, double altura)
+// Função para desenhar o cone
+void drawCone(double raio, double altura)
 {
     double deltaTheta = 2 * PI / p;
-    double deltaZ = altura / (q - 1);
+    double deltaZ = altura / q;
 
     for (int i = 0; i < p; i++)
     {
-        glBegin(GL_QUAD_STRIP);
+        glBegin(GL_TRIANGLE_STRIP);
         for (int j = 0; j <= q; j++)
         {
             double theta = i * deltaTheta;
             double z = j * deltaZ;
-            double x = raio * f(i, j);
-            double y = raio * g(i, j);
+            double x = raio * (1 - (z / altura)) * f(i, j);
+            double y = raio * (1 - (z / altura)) * g(i, j);
 
             double nx = f(i, j);
             double ny = g(i, j);
-            double nz = h(i, j);
+            double nz = (raio / altura) * h(i, j);
 
             glNormal3d(nx, ny, nz);
             glVertex3d(x, y, z);
 
-            x = raio * f(i + 1, j);
-            y = raio * g(i + 1, j);
+            x = raio * (1 - (z / altura)) * f(i + 1, j);
+            y = raio * (1 - (z / altura)) * g(i + 1, j);
 
             nx = f(i + 1, j);
             ny = g(i + 1, j);
-            nz = h(i + 1, j);
+            nz = (raio / altura) * h(i + 1, j);
 
             glNormal3d(nx, ny, nz);
             glVertex3d(x, y, z);
@@ -58,6 +59,7 @@ void drawCylinder(double raio, double altura)
         glEnd();
     }
 }
+
 
 // Função para exibição OpenGL
 void display()
@@ -78,7 +80,7 @@ void display()
     glRotatef(30, 1, 0, 0);
     glRotatef(30, 0, 1, 0);
 
-    drawCylinder(1.0, 2.0);
+    drawCone(1.0, 2.0);
 
     glFlush();
     glutSwapBuffers();
@@ -98,7 +100,7 @@ int main(int argc, char **argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Cylinder");
+    glutCreateWindow("Cone");
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
@@ -109,3 +111,4 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
